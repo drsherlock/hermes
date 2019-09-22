a6ea08d0eae756338537f765c58c350fc96e28bf
drsherlock <zain.delhi27@hotmail.com>
2019-09-22 20:17:42 +0530

    Add random algorithm

--- server.go
+++ server.go
@@ -1,42 +1,48 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "net/http/httputil"
-  "net/url"
-  "encoding/json"
-  "os"
-  "log"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
 )
 
 type Configuration struct {
-  Servers []string
-  Algorithm string
+	Servers   []string
+	Algorithm string
 }
 
 func main() {
-  configFile, err := os.Open("config.json")
-  if err != nil {
-      log.Fatal(err)
-  }
-  defer configFile.Close()
+	configFile, err := os.Open("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer configFile.Close()
 
-  decoder := json.NewDecoder(configFile)
-  configuration := Configuration{}
-  err = decoder.Decode(&configuration)
-  if err != nil {
-    log.Fatal(err)
-  }
+	decoder := json.NewDecoder(configFile)
+	configuration := Configuration{}
+	err = decoder.Decode(&configuration)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-fmt.Println(configuration.Servers)
+	fmt.Println(configuration.Servers)
 
-serverNumber := 0
+	var serverNumber int
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    serverNumber = (serverNumber+1)%len(configuration.Servers)
-    targetUrl, _ := url.Parse(configuration.Servers[serverNumber])
-    httputil.NewSingleHostReverseProxy(targetUrl).ServeHTTP(w, r)
+		if configuration.Algorithm == "RoundRobin" {
+			serverNumber = (serverNumber + 1) % len(configuration.Servers)
+		} else if configuration.Algorithm == "Random" {
+			serverNumber = rand.Intn(len(configuration.Servers))
+		}
+
+		targetUrl, _ := url.Parse(configuration.Servers[serverNumber])
+		httputil.NewSingleHostReverseProxy(targetUrl).ServeHTTP(w, r)
 	})
 
 	http.ListenAndServe(":4000", nil)
